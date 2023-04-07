041e42da560a95dd38d66dbb71351faef58b4da7
Huimin Yu <huiminyu51@gmail.com>
2023-04-06 21:08:20 -0700

    react course project

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {useState, useEffect} from 'react'
+import BlogList from './BlogList';
+import useFetch from './useFetch';
+
+
+const Home = () => {
+    
+    const {error, isPending, data: blogs} = useFetch('http://localhost:8000/blogs');
+
+  return (
+    <div className="home">
+        { error && <div>{error}</div>}
+        { isPending && <div>Loading...</div> }
+        { blogs && <BlogList blogs={ blogs } title='All Blogs' /> }
+    </div>
+  )
+}
+
+export default Home
